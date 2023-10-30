4b1f0eec78f04a01cd40ea3518cd18bd3c4e4f0f
Thomas Nesset <thomasne@uia.no>
2023-10-30 12:45:08 +0100

    Basic face detection done

---
+++ src/main.py
@@ -0,0 +1,20 @@
+import cv2
+import matplotlib.pyplot as plt
+
+imgPath = 'img/id.jpg'
+img = cv2.imread(imgPath)
+
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+face_classifier = cv2.CascadeClassifier(
+    'src/haarcascade_frontalface_default.xml'
+)
+
+face = face_classifier.detectMultiScale(
+    gray_img, scaleFactor=1.1, minNeighbors=5
+)
+
+for (x, y, w, h) in face:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 5)
+
+cv2.imwrite("img/output.jpg", img)
