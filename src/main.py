15d33794686fb2f27e65ddd612954eab879513af
Håvard F. Aasen <havard.f.aasen@pfft.no>
2023-11-13 10:32:41 +0100

    Check image quality and compare IDs
    It is important to have the proper working directory set, to find
    the necessary images.

---
+++ src/main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+from pathlib import Path
+import sys
+
+import cv2
+
+import cli
+import image_verification
+import match
+
+
+if __name__ == "__main__":
+    args = cli.init_argparse()
+
+    have_id = False
+    if args.id_image:
+        if not Path(args.id_image).is_file():
+            print(f"File [{args.id_image}] not found.", file=sys.stderr)
+            exit(1)
+        have_id = True
+
+    if have_id:
+        template_path = Path("images/templates")
+        files = [file for file in template_path.iterdir() if file.is_file()]
+        matched_id = []
+        image = cv2.imread(args.id_image)
+        if not image_verification.quality(image):
+            print("Image quality for ID card is too low.", file=sys.stderr)
+            exit(1)
+
+        for file in files:
+            template = cv2.imread(str(file))
+            score = match.match_orb(image, template, 100, 10)
+            print(f"template: {str(file)}, Score: {score}")
+            if score >= 0.5:
+                matched_id.append({"filename": file, "score": score})
+
+        if matched_id.__len__() == 0:
+            print("Could not match ID.", file=sys.stderr)
+            exit(1)
+
+        best_match = max(matched_id, key=lambda x: x["score"])
+        print(f"ID matched with [{best_match['filename'].name}]")
